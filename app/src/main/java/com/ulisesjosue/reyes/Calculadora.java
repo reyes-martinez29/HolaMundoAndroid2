498eab97aae1301c52ab9a9ccab58f9ef573ab12
reyes-martinez29 <reyes.martinez.ulisesjosue@gmail.com>
2021-11-07 18:42:56 -0600

    Claculadora Version

---
+++ app/src/main/java/com/ulisesjosue/reyes/Calculadora.java
@@ -0,0 +1,16 @@
+package com.ulisesjosue.reyes;
+
+public class Calculadora {
+    public void Suma(){
+
+    }
+    public void Resta(){
+
+    }
+    public void Multiplicacion(){
+
+    }
+    public void Division (){
+
+    }
+}
